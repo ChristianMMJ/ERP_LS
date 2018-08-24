d58e09357574f4620e2659a7b6c91db509e85a32
ChristianMMJ <christian.m.m91@gmail.com>
2018-08-24 11:04:32 -0500

    checkl

--- application/views/vFichaTecnica.php
+++ application/views/vFichaTecnica.php
@@ -84,9 +84,10 @@
                 <div class="col-12 col-sm-12 col-md-4 col-lg-2">
                     <div class="row">
                         <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                            <label for="Consumo">Afecta PV</label>
                             <div class="custom-control custom-checkbox">
                                 <input type="checkbox" class="custom-control-input" id="AfectaPV" name="AfectaPV" >
-                                <label class="custom-control-label" for="AfectaPV">Afecta PV</label>
+                                <label class="custom-control-label" for="AfectaPV"></label>
                             </div>
                         </div>
                         <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
